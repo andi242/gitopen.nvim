f038c175b3317a275cb677f1dc30c2909681632a
andi242 <andi242@gmail.com>
2025-10-19 10:37:02 +0200

    simplify things

--- lua/gitopen/init.lua
+++ lua/gitopen/init.lua
@@ -3,36 +3,34 @@ Opts = {}
 
 -- splitting strings
 local function split(inputstr, sep)
-	sep = sep or '%s'
+	sep = sep or "%s"
 	local t = {}
-	for str in string.gmatch(inputstr, '([^' .. sep .. ']+)') do
+	for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
 		table.insert(t, str)
 	end
 	return t
 end
 
--- parse ssh/http url
+-- parse ssh/http url, return false on error
 local function getWebUrl()
 	local path = vim.fn.getcwd(0, 0)
 	local url
 	local gitcmd = string.format('cd "%s" && git ls-remote --get-url', path)
-	local gitUri = vim.fn.system(gitcmd)
-  -- if @ in url, it's ssh
-	if string.find(gitUri, '@') then
-		local urlArr = split(gitUri, '@')
-		url = urlArr[2]:gsub(':(%d+)/', '/')
-		url = url:gsub(':', '/')
-	else
-		url = gitUri
-	end
-	return url
-end
-
--- check cwd if it is a git repo
-local function isRepo()
-	local check = vim.system({ 'bash', '-c', 'git rev-parse --show-toplevel' }, { text = true }):wait()
+	local check = vim.system({ "bash", "-c", gitcmd }, { text = true }):wait()
 	if check.code == 0 then
-		return true
+		local gitUri = string.format(check.stdout)
+		-- if @ in url, it's ssh
+		if string.find(gitUri, "@") then
+			local urlArr = split(gitUri, "@")
+			url = urlArr[2]:gsub(":(%d+)/", "/")
+			url = url:gsub(":", "/")
+		-- how to strip '.git'? or just ignore?
+		-- url = url.gsub('%.git$', '') -- does not work?
+		-- url = url:sub(0, -6) -- works, but unsafe
+		else
+			url = gitUri
+		end
+		return url
 	else
 		return false
 	end
@@ -40,34 +38,31 @@ end
 
 -- open url handling
 local function openUrl(url)
-  local cmd
-  if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
-    cmd = "start"
-  elseif vim.fn.has("mac") == 1 then
-    cmd = "open"
-  else
-    cmd = "xdg-open"
-  end
-	print('opening https://' .. url)
-  vim.fn.system(cmd .. " https://" .. url)
+	local cmd
+	if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+		cmd = "start"
+	elseif vim.fn.has("mac") == 1 then
+		cmd = "open"
+	else
+		cmd = "xdg-open"
+	end
+	print("opening https://" .. url)
+	vim.fn.system(cmd .. " https://" .. url)
 end
 
-function M.setup()
-  -- not using any for now
-	Opts = {}
-	-- Opts = opts or {}
+function M.setup(opts)
+	-- not using any for now
+	Opts = opts or {}
 end
 
 function M.open()
-	local giturl
-	if isRepo() then
-		giturl = getWebUrl()
-	end
-	if not isRepo() then
-		print('not a git repo.')
+	local giturl = getWebUrl()
+	if giturl then
+		openUrl(giturl)
+	else
+		print("not a git repo.")
 		return
 	end
-  openUrl(giturl)
 end
 
 vim.api.nvim_command('command! GitOpen lua require("gitopen").open()')
